31238ebf8491a252f83a9d39733d144813b8ea03
danielnascimento <danielnastos10@gmail.com[>
2025-11-11 19:32:08 -0300

    att

--- front/src/config/api.ts
+++ front/src/config/api.ts
@@ -1,1 +1,1 @@
-export const API_URL = import.meta.env.VITE_API_URL || "https://grupo12projeto.escolatecnicaadelia.info/api";
+export const API_URL = import.meta.env.VITE_API_URL || "https://grupo12projeto20252.escolatecnicaadelia.info/api";
